ed5eb78f1a96b8f76723312909d7e5e513059fef
lynasellami <2488446@edu.vaniercollege.qc.ca>
2025-05-09 22:04:11 -0400

    Add test for addItem() in Order

--- src/test/java/org/example/OrderTest.java
+++ src/test/java/org/example/OrderTest.java
@@ -1,4 +1,15 @@
 package org.example;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderTest {
+
+
+    @Test
+    public void testAddItemIncreasesSize() {
+        Order order = new Order();
+        Flower flower = new Rose("Red Rose", 10.0, true);
+        order.addItem(flower);
+        assertEquals(1, order.getItems().size());
+    }
 }
