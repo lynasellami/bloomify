fa798ea0d47e7378c5fe4a83c99c210e9c929d50
lynasellami <2488446@edu.vaniercollege.qc.ca>
2025-05-09 22:26:24 -0400

    Add test to check cart contents in Customer

--- src/test/java/org/example/CustomerTest.java
+++ src/test/java/org/example/CustomerTest.java
@@ -11,6 +11,14 @@ public class CustomerTest {
         customer.addToOrder(flower);
         assertEquals(1, customer.getCart().size());
     }
+
+    @Test
+    public void testCartContainsCorrectFlower() {
+        Customer customer = new Customer("Alice", "C001");
+        Flower flower = new Tulip("Yellow Tulip", 5.0, true);
+        customer.addToOrder(flower);
+        assertEquals("Yellow Tulip", customer.getCart().get(0).getName());
+    }
 }
 
 
